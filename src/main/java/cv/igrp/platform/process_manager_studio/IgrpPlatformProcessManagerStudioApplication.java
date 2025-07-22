e517d17f89b21225eb4f778b290c3b080d0d9f11
Ivanick.Santos <ivanick.santos@gov.cv>
2025-07-22 13:00:06 -0100

    initial

---
+++ src/main/java/cv/igrp/platform/process_manager_studio/IgrpPlatformProcessManagerStudioApplication.java
@@ -0,0 +1,30 @@
+package cv.igrp.platform.process_manager_studio;
+
+import cv.igrp.platform.process_manager_studio.shared.config.ApplicationAuditorAware;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAware", dateTimeProviderRef = "auditDateTimeProvider")
+public class IgrpPlatformProcessManagerStudioApplication {
+
+  @Bean
+  public AuditorAware<String> auditAware() {
+    return new ApplicationAuditorAware();
+  }
+
+  @Bean
+  public DateTimeProvider auditDateTimeProvider() {
+    return () -> Optional.of(LocalDateTime.now());
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(IgrpPlatformProcessManagerStudioApplication.class, args);
+  }
+}
---
+++ src/test/java/cv/igrp/platform/process_manager_studio/IgrpPlatformProcessManagerStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package cv.igrp.platform.process_manager_studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IgrpPlatformProcessManagerStudioApplicationTests {
+
+  @Test
+  public void contextLoads() {
+
+  }
+}
