2b20bd8c096caa512b3aec6ccd5886fd22f52925
Ivanick.Santos <ivanick.santos@gov.cv>
2025-07-22 20:39:07 -0100

    Add pre-defined exception methods for common HTTP status codes
    - Introduced `notFound`, `badRequest`, and `internalServerError` methods in `IgrpResponseStatusException` for easier exception handling.
    - Each method initializes `ProblemDetail` and sets appropriate titles and HTTP status codes.

--- src/main/java/cv/igrp/platform/process_manager_studio/shared/domain/exceptions/IgrpResponseStatusException.java
+++ src/main/java/cv/igrp/platform/process_manager_studio/shared/domain/exceptions/IgrpResponseStatusException.java
@@ -33,4 +33,23 @@ public class IgrpResponseStatusException extends ErrorResponseException {
         problemDetail.setProperties(Map.of("details", details));
         return new IgrpResponseStatusException(status, problemDetail, null);
     }
+
+  public static IgrpResponseStatusException notFound(String title) {
+    var problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+    problemDetail.setTitle(title);
+    return new IgrpResponseStatusException(HttpStatus.NOT_FOUND, problemDetail, null);
+  }
+  public static IgrpResponseStatusException badRequest(String title) {
+    var problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+    problemDetail.setTitle(title);
+    return new IgrpResponseStatusException(HttpStatus.BAD_REQUEST, problemDetail, null);
+  }
+
+  public static IgrpResponseStatusException internalServerError(String title) {
+    var problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+    problemDetail.setTitle(title);
+    return new IgrpResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, problemDetail, null);
+  }
+
+
 }
