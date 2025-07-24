19923c82d9230cc8b97c92005f87bb21f814e78c
Ivanick.Santos <ivanick.santos@gov.cv>
2025-07-24 12:28:16 -0100

    Introduce `ActivitiDeploymentAdapter` and `ProcessDeploymentPort` with UUID dependency
    - Added `ActivitiDeploymentAdapter` class implementing `ProcessDeploymentPort` interface.
    - Updated `pom.xml` to generalize IGRP framework versions and added `uuid-creator` dependency for UUID generation.

---
+++ src/main/java/cv/igrp/platform/process_manager_studio/project/domain/ports/ProcessDeploymentPort.java
@@ -0,0 +1,4 @@
+package cv.igrp.platform.process_manager_studio.project.domain.ports;
+
+public interface ProcessDeploymentPort {
+}
---
+++ src/main/java/cv/igrp/platform/process_manager_studio/project/infrastructure/adapter/ActivitiDeploymentAdapter.java
@@ -0,0 +1,8 @@
+package cv.igrp.platform.process_manager_studio.project.infrastructure.adapter;
+
+import cv.igrp.platform.process_manager_studio.project.domain.ports.ProcessDeploymentPort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivitiDeploymentAdapter implements ProcessDeploymentPort {
+}
